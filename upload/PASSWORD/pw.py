055eafe6e2432962010215c47ef563e8a206273d
Mike Kao <skybow@gmail.com>
2020-04-13 00:33:31 +0800

    V3, add user define range

--- upload/PASSWORD/pw.py
+++ upload/PASSWORD/pw.py
@@ -1,9 +1,13 @@
 import random
 COUNT = 0
-PW = input('PASSWORD=? ')
-PW = int(PW)
-RPW = random.randint(1, 100)
+START = input('START FROM ')
+START = int(START)
+END = input('END AT ')
+END = int(END)
+RPW = random.randint(START, END)
 while True:
+  PW = input('PASSWORD=? ')
+  PW = int(PW)
   COUNT = COUNT + 1
   if PW == RPW:
     print('YOU GOT IT! TRIED',COUNT,'TIMES!')
