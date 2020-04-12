76cb83e47b3b7626910a7156991396428dcb57e7
Mike Kao <skybow@gmail.com>
2020-04-13 00:23:45 +0800

    V2, add count

--- upload/PASSWORD/pw.py
+++ upload/PASSWORD/pw.py
@@ -1,14 +1,15 @@
 import random
-
+COUNT = 0
 PW = input('PASSWORD=? ')
 PW = int(PW)
 RPW = random.randint(1, 100)
-
 while True:
+  COUNT = COUNT + 1
   if PW == RPW:
-    print('YOU GOT IT!')
+    print('YOU GOT IT! TRIED',COUNT,'TIMES!')
     break
   elif PW > RPW:
     print('BIGGER THAN ANSWER')
   elif PW < RPW:
     print('LESS THAN ANSWER')
+  print('TRIED',COUNT,'TIMES!')
